4e449933d71fe3729feb20f6237c34e6ab75e52c
joao aguiar <joao_aguiarcosta@hotmail.com>
2025-03-24 15:21:25 -0300

    preProcessamento completo

--- preProcessamento/app.py
+++ preProcessamento/app.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import pytesseract
 
 image = cv2.imread("preProcessamento/src/teste.jpg")
 
@@ -46,6 +47,66 @@ adapt_gauss = cv2.adaptiveThreshold(invert, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
 cv2.imshow("negative_gauss", adapt_gauss)
 
 #operacoes morfológicas - erosão (diminui a quantidade de pixels brancos do objeto) e dilatação (aumenta quantidade de pixels brancos do objeto)
+image = cv2.imread("preProcessamento/src/morfologica.png")
+cv2.imshow("morfologica", image)
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow("gray", gray)
+
+""" image = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 3, 9)
+cv2.imshow("image", image) """
+
+#erosao - método erode para erosão, recebe uma matriz de 1s para fazer comparação entre os pixels e definir como será feita essa erosão de imagem. Ao redor de cada pixel é verificado com a matriz definida(no caso uma 3x3),  se um dos pixels nessa janela for identificado como preto, o pixel central se tornará preto também.
+erosao = cv2.erode(gray, np.ones((5,5), np.uint8))
+cv2.imshow('erosao', erosao)
+
+#dilatacao - funciona de forma semelhante ao método de erosão, mas ao inves de tornar os pixels em preto, ele verifica se há pixels vizinhos brancos e transforma os centrais em brancos
+dilatacao = cv2.dilate(gray, np.ones((3,3), np.uint8))
+cv2.imshow("dilatacao", dilatacao)
+
+#fazendo uma erosao para eliminar ruido e logo apos uma dilatação, é possível voltar ao estado anterios da imagem mas sem os ruídos existentes antes. Esse seria o método de abertura
+erosaoDilatacao = cv2.dilate(erosao, np.ones((5,5), np.uint8))
+cv2.imshow("erosaoDilatacao", erosaoDilatacao)
+
+#blur para remoção de ruido
+ruido = cv2.imread("preProcessamento/src/imageRuido.jpg")
+cv2.imshow("original", ruido)
+
+ruidoAmpliado = cv2.resize(ruido, None, fx=1.8, fy=1.8, interpolation=1)
+cv2.imshow("ampliado", ruidoAmpliado)
+
+gray = cv2.cvtColor(ruidoAmpliado, cv2.COLOR_BGR2GRAY)
+
+mean_blur = cv2.blur(gray, (5,5))
+cv2.imshow("mean_blur", mean_blur)
+
+gaussBlur = cv2.GaussianBlur(gray, (5,5), 0)
+cv2.imshow("gaussBlur", gaussBlur)
+
+#o segundo argumento é o tipo de matriz. Nesse caso é uma 3x3 usada
+median_blur = cv2.medianBlur(gray, 3)
+cv2.imshow("median_blur", median_blur)
+
+bilateral_blur = cv2.bilateralFilter(ruidoAmpliado, 80,55,45)
+cv2.imshow("bilateral_blur", bilateral_blur)
+
+exercicio = cv2.imread("preProcessamento/src/exercicio.webp")
+cv2.imshow("exercicio", exercicio)
+
+exercicio = cv2.cvtColor(exercicio, cv2.COLOR_BGR2GRAY)
+cv2.imshow("exercicio", exercicio)
+
+val, thresh = cv2.threshold(exercicio, 180, 255, cv2.THRESH_BINARY)
+cv2.imshow("thresh", thresh)
+
+invert = 255 - thresh
+cv2.imshow("invert", invert)
+
+
+config_tesseract = '--oem 3 --psm 6'
+text = pytesseract.image_to_string(invert, config=config_tesseract, lang="por")
+print(text)
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
